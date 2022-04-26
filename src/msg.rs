212953cce2b38aaab5a7f071984622b68b0ae4e6
Martin Iglesias <72715575+MartinIglesias86@users.noreply.github.com>
2022-04-26 18:56:41 -0300

    Second commit

---
+++ src/msg.rs
@@ -0,0 +1,35 @@
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+use cosmwasm_std:: Addr;
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct InstantiateMsg {
+    pub speed: i32,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum ExecuteMsg {
+    UpsertScore { score: u16 }, // This will add or update scores
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum QueryMsg {
+    // GetSpeed returns the current speed as a json-encoded number
+    GetSpeed {},
+    // Just like with GetSpeed, we declare an enum for scores too
+    GetScores {},
+}
+
+// We define a custom struct for each query response
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct SpeedResponse {
+    pub speed: i32,
+}
+
+// Here's the response type for our scores, it's a vector of the same object type we defined in state.rs
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct ScoreResponse{
+    pub scores: Vec<(Addr, u16)>,
+}
